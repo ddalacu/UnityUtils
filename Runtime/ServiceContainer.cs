15261f1a38faae686f13de3972f76cc6e5c57e87
Diac Paul <diac.paul@quadom.com>
2020-03-25 15:30:47 +0200

    Service container

--- Runtime/ServiceContainer.cs
+++ Runtime/ServiceContainer.cs
@@ -15,11 +15,36 @@ public class ServiceContainer
         _serviceMapLock = new object();
     }
 
+    public void RemoveService<TServiceContract>(TServiceContract implementation) where TServiceContract : class
+    {
+        lock (_serviceMapLock)
+        {
+            if (_serviceMap.TryGetValue(typeof(TServiceContract), out var result) && result.Equals(implementation))
+                _serviceMap.Remove(typeof(TServiceContract));
+        }
+    }
+
     public void AddService<TServiceContract>(TServiceContract implementation) where TServiceContract : class
     {
         lock (_serviceMapLock)
         {
             _serviceMap[typeof(TServiceContract)] = implementation;
+        }
+    }
+
+    public bool FillService<TServiceContract>(out TServiceContract result) where TServiceContract : class
+    {
+        lock (_serviceMapLock)
+        {
+            object service;
+            if (_serviceMap.TryGetValue(typeof(TServiceContract), out service))
+            {
+                result = service as TServiceContract;
+                return result != null;
+            }
+
+            result = null;
+            return false;
         }
     }
 
