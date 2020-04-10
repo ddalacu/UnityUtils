de6cb69b5ec6e791e30e0f8ac666ab5c23d1e4ef
Diac Paul <diac.paul@quadom.com>
2020-04-10 14:40:36 +0300

    Scriptable bool ui toggle

---
+++ Runtime/ScriptableBoolToggle.cs
@@ -0,0 +1,39 @@
+﻿using Framework.Utility;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptableBoolToggle : MonoBehaviour
+{
+    [SerializeField]
+    private ScriptableBoolValue _scriptableBool;
+
+    [SerializeField]
+    private Toggle _toggle;
+
+    private void Awake()
+    {
+        _toggle.onValueChanged.AddListener(Toggled);
+        _scriptableBool.ValueChanged += ValChanged;
+    }
+
+    private void OnDestroy()
+    {
+        if (_toggle)
+            _toggle.onValueChanged.RemoveListener(Toggled);
+        _scriptableBool.ValueChanged -= ValChanged;
+    }
+
+    private void ValChanged(IObservableValue<bool> observable)
+    {
+        _toggle.onValueChanged.RemoveListener(Toggled);
+        _toggle.isOn = observable.Value;
+        _toggle.onValueChanged.AddListener(Toggled);
+    }
+
+    private void Toggled(bool value)
+    {
+        _scriptableBool.ValueChanged -= ValChanged;
+        _scriptableBool.Value = value;
+        _scriptableBool.ValueChanged += ValChanged;
+    }
+}
