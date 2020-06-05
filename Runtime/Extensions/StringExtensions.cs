ff4b1b733df6a64d221f3642877580660538c7e1
Diac Paul <diac.paul@quadom.com>
2020-06-05 09:30:40 +0300

    byte array to hex

--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -118,4 +118,12 @@ public static class StringExtensions
         }
     }
 
+    public static string ByteArrayToHexString(byte[] ba)
+    {
+        StringBuilder hex = new StringBuilder(ba.Length * 2);
+        foreach (byte b in ba)
+            hex.AppendFormat("{0:x2}", b);
+        return hex.ToString();
+    }
+
 }
